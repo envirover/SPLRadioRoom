672abb76960d93b6fbfdde65a0819f73575c860f
Pavel Bobov <pb@envirover.com>
2018-01-04 19:57:56 -0800

    Updated license header in Stopwatch.h

--- src/Stopwatch.h
+++ src/Stopwatch.h
@@ -1,19 +1,25 @@
 /*
- * Envirover confidential
- *
- *  [2017] Envirover
- *  All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains the property of
- * Envirover and its suppliers, if any.  The intellectual and technical concepts
- * contained herein are proprietary to Envirover and its suppliers and may be
- * covered by U.S. and Foreign Patents, patents in process, and are protected
- * by trade secret or copyright law.
- *
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Envirover.
- */
+ Stopwatch.h
+
+ (C) Copyright 2018 Envirover.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+ Created on: Jan 4, 2018
+     Author: Pavel Bobov
+*/
 
 #ifndef STOPWATCH_H_
 #define STOPWATCH_H_
